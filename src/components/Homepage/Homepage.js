fde685c5263e5a0b4a36a8fe09edd570e5e9c1b7
Deividas K <kasetukas123@gmail.com>
2024-01-30 16:25:54 +0200

    update

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -30,14 +30,14 @@ const Homepage = () => {
             <div className="">
                 <div className="flex md:px-28 justify-center items-center h-screen w-screen">
                     <div className=" p-2 text-center">
-                        <h2 className="mockupTitle mt-10 md:text-[65px] text-[50px] animate__animated animate__zoomIn animate__delay-1s">We Building Tomorrow's Digital Landscape Today</h2>
-                        <h2 className="mockupTitle mt-7 text-[25px] leading-7 md:text-[26px] animate__animated animate__zoomIn animate__delay-1s">Everything from simple tasks to worldwide projects</h2>
-                        <button className="button mt-10 animate__animated animate__zoomIn animate__delay-1s" onClick={() => scrollToTarget()}>Launch Your Project</button>
+                        <h2 className="mockupTitle mt-10 md:text-[75px] text-[50px] animate__animated animate__zoomIn animate__delay-1s leading-[75px]">We Building Tomorrow's Digital Landscape Today</h2>
+                        <h2 className="mockupTitle mt-7 text-[25px] leading-7 md:text-[35px] animate__animated animate__zoomIn animate__delay-1s">Everything from simple tasks to worldwide projects</h2>
+                        <button className="button mt-16 animate__animated animate__zoomIn animate__delay-1s" onClick={() => scrollToTarget()}>Launch Your Project</button>
                     </div>
                 </div>
-                <div className="md:px-28 md:mt-20">
+                {/* <div className="md:px-28 md:mt-20">
                     <ServicesCards />
-                </div>
+                </div> */}
                 <div className="md:px-28 md:mt-20">
                     <WhyChooseUs />
                 </div>
