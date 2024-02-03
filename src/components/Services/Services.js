f67dd40fec724805e5fa50cdc58e9a2f7f98c282
Deividas K <kasetukas123@gmail.com>
2024-02-03 16:50:35 +0200

    update

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,28 +1,93 @@
 import "../Services/Services.scss"
+import Logo from "../../assets/image/favicon.png"
+import { useState } from "react";
+import { FaAngleDown } from "react-icons/fa6";
+import LetsConnect from "../LetsConnect/LetsConnect"
+
+
+const AccordionItem = ({ title, content, index, isOpen, toggleAccordion, description }) => (
+    <div className="relative mb-3">
+        <div className="mb-0 bg-[#00416a8e] h-[250px] m-5">
+            <button className="relative w-full p-4 text-left cursor-pointer" onClick={() => toggleAccordion(index)}>
+                <div className="servicesText">
+                    <span className="servicesText text-4xl">{title}</span>
+                    <p className="text-xl mt-5">{description}</p>
+                </div>
+                <div className="flex justify-end">
+                    <p className="text-white flex text-xl mt-10">Learn More <span><FaAngleDown className="text-[30px] mt-1 ml-1" /></span></p>
+                </div>
+            </button>
+        </div>
+        <div className={`overflow-hidden ${isOpen ? 'h-auto mx-5 bg-[#00416a] animate__animated animate__slideInDown' : 'h-0'}`}>
+            <div className="p-4 leading-normal text-blue-gray-500/80 text-lg text-white animate__animated animate__slideInLeft">{content}</div>
+        </div>
+    </div>
+);
 
 
 const Services = () => {
+
+    const [openItems, setOpenItems] = useState([]);
+
+    const toggleAccordion = (index) => {
+        setOpenItems((prevOpenItems) =>
+            prevOpenItems.includes(index)
+                ? prevOpenItems.filter((item) => item !== index)
+                : [...prevOpenItems, index]
+        );
+    };
+
     return (
-        <div className="h-screen relative md:px-[20%] px-10 md:text-left text-center">
-            <div className="md:pt-[20%] pt-[35%]">
-                <div className="md:w-[60%]">
-                    <h1 className="servicesText text-6xl">Future-Ready IT Solutions for Today's Challenges</h1>
-                    <p className="servicesText text-xl">Elevate your business operations to the next level with our comprehensive IT services. We focus on delivering seamless and efficient solutions, including network management, system integration, and 24/7 support. Experience unparalleled reliability and performance with our advanced IT offerings.</p>
-                </div>
-                <div className="">
-                    <div class="loaderService md:right-[5%] md:top-[50%] left-[50%] top-[125%]">
-                        <div class="loaderService-cube">
-                            <div class="faceService"></div>
-                            <div class="faceService"></div>
-                            <div class="faceService"></div>
-                            <div class="faceService"></div>
-                            <div class="faceService"></div>
-                            <div class="faceService"></div>
-                        </div>
+        <>
+            <div className="relative md:px-[10%] px-5 md:text-left text-center">
+                <div className="md:pt-[20%] pt-[35%] md:flex">
+                    <div className="md:w-[90%]">
+                        <h1 className="servicesText text-6xl">Future-Ready IT Solutions for Today's Challenges</h1>
+                        <p className="servicesText text-xl mt-6">Elevate your business operations to the next level with our comprehensive IT services. We focus on delivering seamless and efficient solutions, including network management, system integration, and 24/7 support. Experience unparalleled reliability and performance with our advanced IT offerings.</p>
+                    </div>
+                    <div className="px-20 md:mt-0 mt-10 text-center">
+                        <img className="md:w-[600px] " src={Logo} />
                     </div>
                 </div>
+                <div className="mt-20 grid grid-row md:grid-cols-2">
+                    <AccordionItem
+                        title="Custom Web Development"
+                        description="We build robust, scalable websites that serve..."
+                        content="Tailored web solutions that align perfectly with your business objectives. We build robust, scalable websites that serve as powerful tools for your online presence."
+                        index={1}
+                        isOpen={openItems.includes(1)}
+                        toggleAccordion={toggleAccordion}
+                    />
+                    <AccordionItem
+                        title="Mobile App Solutions"
+                        description="We prioritize user experience in every aspect of app development..."
+                        content="We prioritize user experience in every aspect of app development, ensuring that your application not only meets but exceeds user expectations."
+                        index={2}
+                        isOpen={openItems.includes(2)}
+                        toggleAccordion={toggleAccordion}
+                    />
+                    <AccordionItem
+                        title="Development Consulting"
+                        description="Our seasoned team of web development consultants is dedicated to..."
+                        content="Our seasoned team of web development consultants is dedicated to guiding you through the intricacies of the digital landscape, offering tailored solutions to elevate your business to new heights."
+                        index={3}
+                        isOpen={openItems.includes(3)}
+                        toggleAccordion={toggleAccordion}
+                    />
+                    <AccordionItem
+                        title="Custom Web Development"
+                        description="We build robust, scalable websites that serve..."
+                        content="Tailored web solutions that align perfectly with your business objectives. We build robust, scalable websites that serve as powerful tools for your online presence."
+                        index={4}
+                        isOpen={openItems.includes(4)}
+                        toggleAccordion={toggleAccordion}
+                    />
+                </div>
             </div>
-        </div>
+            <div>
+                <LetsConnect />
+            </div>
+        </>
     )
 }
 
