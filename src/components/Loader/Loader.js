c003d988e7c93b7bc55ba1d60724213300c16c48
Deividas K <kasetukas123@gmail.com>
2024-02-05 22:08:26 +0200

    update

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,35 +1,41 @@
 import "../Loader/Loader.scss"
+import Logo from "../../assets/image/toruslogo.png"
 
 const Loader = () => {
     return (
         <div className="w-screen h-screen flex justify-center items-center bg-[#00416a]">
-            <div class="cubeloader">
-                <div class="box box0">
-                    <div></div>
+            <div>
+                <div class="cubeloader ml-20">
+                    <div class="box box0">
+                        <div></div>
+                    </div>
+                    <div class="box box1">
+                        <div></div>
+                    </div>
+                    <div class="box box2">
+                        <div></div>
+                    </div>
+                    <div class="box box3">
+                        <div></div>
+                    </div>
+                    <div class="box box4">
+                        <div></div>
+                    </div>
+                    <div class="box box5">
+                        <div></div>
+                    </div>
+                    <div class="box box6">
+                        <div></div>
+                    </div>
+                    <div class="box box7">
+                        <div></div>
+                    </div>
+                    <div class="ground">
+                        <div></div>
+                    </div>
                 </div>
-                <div class="box box1">
-                    <div></div>
-                </div>
-                <div class="box box2">
-                    <div></div>
-                </div>
-                <div class="box box3">
-                    <div></div>
-                </div>
-                <div class="box box4">
-                    <div></div>
-                </div>
-                <div class="box box5">
-                    <div></div>
-                </div>
-                <div class="box box6">
-                    <div></div>
-                </div>
-                <div class="box box7">
-                    <div></div>
-                </div>
-                <div class="ground">
-                    <div></div>
+                <div className="relative mt-[-100px]">
+                    <img className="h-[100px]" src={Logo}></img>
                 </div>
             </div>
         </div>
